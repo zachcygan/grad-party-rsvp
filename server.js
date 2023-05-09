6d42767638eb2676e42890797ec0647c7f144bb7
zachcygan <zachcygan@gmail.com>
2023-05-09 15:38:28 -0700

    added handlebars

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 const db = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const hbs = exphbs.create({ helpers })
+
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 db.once('open', () => {
