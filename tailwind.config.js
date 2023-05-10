61394f8d4eaa09488cec9c37661562294f4e0532
zachcygan <zachcygan@gmail.com>
2023-05-10 14:02:38 -0700

    changed colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
 module.exports = {
-  content: ['./views/**/*.handlebars',
-            './public/**/*.{css,js}'],
+  content: ['./views/**/*.handlebars', './public/**/*.{css,js}'],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
+      colors: {
+        'light-blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+        'white': '#FFFFFF',
+        'navy-blue': '#003366',
+        'orange': '#FF5722'
+      },
       spacing: {
         '8xl': '96rem',
         '9xl': '128rem',
@@ -25,6 +22,10 @@ module.exports = {
       borderRadius: {
         '4xl': '2rem',
       }
-    }
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
   },
 }
